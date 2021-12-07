55a5005b4f86a1952b07ebe35459b27df19282ce
Rohith Srinivaas M <rohithsrinivaas@gmail.com>
2021-12-08 00:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pickle
 st.write("""
 # Credit Card Approval Prediction App
 
-This app predicts the credit card Default probablity
+This app predicts the credit card approval probablity
 """)
 #Get Input
 
